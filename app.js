5c8bedcbd31c2873f964214c83e73beb8dfbd572
Franco <franco.peris@gmail.com>
2023-05-27 03:24:08 -0300

    .

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
-
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Servidor corriendo en puerto 3000')
-})
+const port = process.env.PORT || 3000;
+app.listen(port ,() => console.log(`Servidor corriendo en el puerto ${port}`))
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
